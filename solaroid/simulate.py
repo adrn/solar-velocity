77a694c8b5ab689c71ab7b0718554825c922d40a
Adrian Price-Whelan <adrian.prw@gmail.com>
2022-02-20 12:07:34 -0500

    more robust simulator

--- solaroid/simulate.py
+++ solaroid/simulate.py
@@ -1,25 +1,33 @@
 import astropy.units as u
 import numpy as np
-from scipy.integrate import quad
 from scipy.interpolate import interp1d
+from .integrate import log_simpson
 
 
-def invt_sample_z(density, size=1, 
-                  grid_lim=[-5, 5]*u.kpc, grid_size=256, 
+def invt_sample_z(log_density, size=1,
+                  grid_lim=[-5, 5]*u.kpc, grid_size=1024,
                   args=(), rng=None):
     """
     Inverse transform sampling to generate "true" z values for simulation:
     """
     if rng is None:
         rng = np.random.default_rng()
-    
+
     zgrid = np.linspace(*grid_lim.to_value(u.pc), grid_size)
-    pdf = density(zgrid, *args)
-    cdf = np.array([
-        quad(density, -np.inf, zz, args=args, limit=1024, epsabs=1e-10)[0] 
-        for zz in zgrid
+    grid_step = zgrid[1] - zgrid[0]
+    zgrid = np.concatenate((
+        np.arange(zgrid[0] - 3 * grid_step, zgrid[0], grid_step),
+        zgrid
+    ))
+
+    log_pdf = log_density(zgrid, *args)
+
+    log_cdf = np.array([
+        log_simpson(log_pdf[:i], x=zgrid[:i])
+        for i in range(3, len(zgrid))
     ])
-    
-    interp_f = interp1d(cdf, zgrid, kind='cubic')
+    cdf = np.exp(log_cdf)
+
+    interp_f = interp1d(cdf, zgrid[3:], kind='cubic')
     z_samples = interp_f(rng.uniform(cdf.min(), cdf.max(), size=size))
     return z_samples
