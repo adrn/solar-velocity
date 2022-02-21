d83ce912c246bcf9e51b17c67d3451679d4a1bcd
Adrian Price-Whelan <adrian.prw@gmail.com>
2022-02-21 18:56:03 -0500

    make a figure

--- src/figures/SgrAstar-PM.py
+++ src/figures/SgrAstar-PM.py
@@ -1,5 +1,6 @@
 import pathlib
 import arviz as az
+import corner
 
 
 this_path = pathlib.Path(__file__).parent
@@ -9,4 +10,12 @@ samples = {}
 for name in ['jointNorth', 'jointEast']:
     samples[name] = az.from_netcdf(data_path / f'Reid2020_{name}.netcdf')
 
-print(samples)
+data = {
+    'pmra': samples['jointEast'].posterior.pm.values.ravel(),
+    'pmdec': samples['jointNorth'].posterior.pm.values.ravel(),
+    'accra': samples['jointEast'].posterior.acc.values.ravel(),
+    'accdec': samples['jointNorth'].posterior.acc.values.ravel()
+}
+
+fig = corner.corner(data)
+fig.savefig('SgrAstar-PM.pdf')
