ca23796e19221a4ea174142fe430375335510949
Adrian Price-Whelan <adrian.prw@gmail.com>
2022-02-22 22:12:05 -0500

    split out stats stuff into stats module

--- solaroid/likelihood.py
+++ solaroid/likelihood.py
@@ -2,24 +2,7 @@ import astropy.units as u
 import numpy as np
 from .coordinates import gal_to_schmagal
 from .integrate import log_simpson
-
-
-def ln_normal(x, mu, std):
-    return -0.5 * (x-mu)**2 / std**2 - 0.5*np.log(2*np.pi) - np.log(std)
-
-
-def ln_uniform(x, a, b):
-    return -np.log(b - a)
-
-
-def ln_two_sech2(z, h1, h2, f):
-    lnterm1 = np.log(f) - 2 * np.log(np.cosh(z / (2 * h1))) - np.log(4 * h1)
-    lnterm2 = np.log(1 - f) - 2 * np.log(np.cosh(z / (2 * h2))) - np.log(4 * h2)
-    return np.logaddexp(lnterm1, lnterm2)
-
-
-def ln_exp(x, x0, h):
-    return - (x-x0) / h - np.log(h)
+from .stats import ln_two_sech2
 
 
 def ln_density(
---
+++ solaroid/stats.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+
+def ln_normal(x, mu, std):
+    return -0.5 * (x-mu)**2 / std**2 - 0.5*np.log(2*np.pi) - np.log(std)
+
+
+def ln_uniform(x, a, b):
+    return -np.log(b - a)
+
+
+def ln_two_sech2(z, h1, h2, f):
+    lnterm1 = np.log(f) - 2 * np.log(np.cosh(z / (2 * h1))) - np.log(4 * h1)
+    lnterm2 = np.log(1 - f) - 2 * np.log(np.cosh(z / (2 * h2))) - np.log(4 * h2)
+    return np.logaddexp(lnterm1, lnterm2)
+
+
+def ln_exp(x, x0, h):
+    return - (x-x0) / h - np.log(h)
