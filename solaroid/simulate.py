6d5630602441158e1de9ef3d9bf3395ceef2b299
Adrian Price-Whelan <adrian.prw@gmail.com>
2022-02-26 11:48:41 -0500

    improve invt sampling over a domain

--- solaroid/simulate.py
+++ solaroid/simulate.py
@@ -10,22 +10,22 @@ from scipy.interpolate import interp1d
 from .integrate import log_simpson
 
 
-def get_log_cdf(log_density, grid_lim=[-5, 5]*u.kpc, grid_size=1024, args=()):
-    zgrid = np.linspace(*grid_lim.to_value(u.pc), grid_size)
-    grid_dz = zgrid[1] - zgrid[0]
+def get_log_cdf(log_density, grid, args=()):
+    grid_dz = grid[1] - grid[0]
 
-    log_pdf = log_density(zgrid, *args)
+    normalize, err = quad(
+        lambda z: np.exp(log_density(z, *args)),
+        grid[0], grid[-1], epsabs=1e-10
+    )
+    log_normalize = np.log(normalize)
 
+    log_pdf = log_density(grid, *args) - log_normalize
     log_cdf = np.array([
         log_simpson(log_pdf[:i], dx=grid_dz, even='last')
-        for i in range(1, len(zgrid) + 1)
+        for i in range(1, len(grid) + 1)
     ])
 
-    lower, err = quad(lambda z: np.exp(log_density(z, *args)),
-                      -np.inf, zgrid[0], epsabs=1e-10)
-    log_cdf = np.logaddexp(np.log(lower), log_cdf)
-
-    return zgrid * u.pc, log_cdf
+    return grid, log_cdf
 
 
 def get_cdf_quad(log_density, grid_lim=[-5, 5]*u.kpc, grid_size=1024, args=()):
